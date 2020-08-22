2aafadd013eb37587809584f4dad0699928b3092
chenjunyu <389965626@qq.com>
2020-08-22 11:47:03 +0800

    Reactor模式+ThreadPool框架

---
+++ test_server.cpp
@@ -0,0 +1,78 @@
+
+#include "TcpServer.h"
+#include "Threadpool.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+class Task
+{
+public:
+	Task(const string & query, const wd::TcpConnectionPtr & conn)
+	: _queury(query)
+	, _conn(conn)
+	{}
+	
+	//process的执行是在一个计算线程里面完成的
+	void process()
+	{
+		cout << "> task is processing" << endl;
+		//_conn->send(_queury);//如果直接去使用send，在计算线程里面完成了数据发送,不可取
+		_conn->sendInLoop(_queury);
+	}
+private:
+	string _queury;
+	wd::TcpConnectionPtr _conn;
+};
+
+wd::Threadpool * g_threadpool = NULL;
+
+void onConnection(const wd::TcpConnectionPtr &conn)
+{
+	cout << conn->toString() << endl;
+    conn->send("hello, welcome to Chat Server.\r\n");
+}
+
+//运行在IO线程
+void onMessage(const wd::TcpConnectionPtr &conn)
+{
+    std::string s(conn->receive());
+
+	Task task(s, conn);
+	g_threadpool->addTask(std::bind(&Task::process, task));
+	cout << "> add task to threadpool" << endl;
+}
+
+void onClose(const wd::TcpConnectionPtr &conn)
+{
+    printf("%s close\n", conn->toString().c_str());
+}
+
+//class EchoServer
+//{
+//public:
+//	void start();
+//	void onMessage();
+//	void onConnection();
+//	void onClose();
+//private:
+//	Threadpool _threadpool;
+//	TcpServer _server;
+//};
+
+int main(int argc, char const *argv[])
+{
+	wd::Threadpool threadpool(4, 10);
+	g_threadpool = &threadpool;
+	threadpool.start();
+
+	wd::TcpServer tcpserver("172.25.214.250", 9999);
+	tcpserver.setConnectionCallback(&onConnection);
+	tcpserver.setMessageCallback(&onMessage);
+	tcpserver.setCloseCallback(&onClose);
+
+	tcpserver.start();
+
+    return 0;
+}
